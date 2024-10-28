456d158bebaa7770e1dc8c3cd20189dfee8a6ceb
Jeptoo bridget <jeptoobridgid007@gmail.com>
2024-10-28 18:40:48 +0300

    initial

---
+++ boolean.dart
@@ -0,0 +1,7 @@
+//boolean are true or false
+
+void main() {
+  bool isover18 = true;
+  print(isover18);
+  print(100 < 10);
+}
---
+++ variable.dart
@@ -0,0 +1,7 @@
+void main() {
+  var name = "bree";
+  var hobby = "coding";
+  var age = 22;
+
+  print("my name is $name i like $hobby and i am $age");
+}
